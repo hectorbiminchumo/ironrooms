4d288d69f48dc7cbade09ad32417ac676459314a
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-11-26 12:04:33 -0500

    integracion de tailwind

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+// ./index.js
 // 1. IMPORTACIONES
 const express 	= require("express")
-
 const app		= express()
-const hbs       = require("hbs")
+const hbs		= require("hbs")
+
 const connectDB = require("./config/db")
 
 require("dotenv").config()
 
 // 2. MIDDLEWARES
 app.use(express.static("public"))
+
 app.set("views", __dirname + "/views")
 app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
 
 connectDB()
 
 // 3. RUTAS
 app.use("/", require("./routes/index"))
+
+
 
 // 4. SERVIDOR
 app.listen(process.env.PORT, () => {
